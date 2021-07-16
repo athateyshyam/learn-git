9aa4dbb065ec49d9e384f5bfac47bf539bc29cef
athateyshyam <athateyshyam@gmail.com>
2021-07-16 04:27:25 -0700

    First-Init Commit

---
+++ src/com/learn/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.learn;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("This is your first java program");
+		System.out.println("Hello world...!!");
+	}
+
+}
