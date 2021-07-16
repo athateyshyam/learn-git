1d49abfb04288dc40be7b4542d4b6d78bf95d627
athateyshyam <athateyshyam@gmail.com>
2021-07-16 04:28:57 -0700

    New Changes

--- src/com/learn/HelloWorld.java
+++ src/com/learn/HelloWorld.java
@@ -2,7 +2,6 @@ package com.learn;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("This is your first java program");
 		System.out.println("Hello world...!!");
 	}
 
