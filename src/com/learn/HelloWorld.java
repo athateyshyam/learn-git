2032292b07aa6ce78b3151da0ebb5b1423f02e00
slowchessblitz <slowchessblitz@gmail.com>
2021-07-25 02:10:02 -0700

    New changes added by slow blitz

--- src/com/learn/HelloWorld.java
+++ src/com/learn/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world...!!");
 		System.out.println("This is first code...!!");
+		System.out.println("I am new to this repository");
 	}
 
 }
